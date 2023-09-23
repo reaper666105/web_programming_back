a50356ae29b1306084929ebe1784041274e971c6
Студент Факультета Бизнеса <ka.father@mail.ru>
2023-09-23 15:37:51 +0700

    'commit_2'

--- app.py
+++ app.py
@@ -4,5 +4,23 @@ app=Flask(__name__)
 
 @app.route('/')
 def start():
-    return "web-сервер на flask"
+    return """
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Юшков. Лабораторная 1</title> 
+    </head>
+    <body>
+        <header> 
+            НГТУ, ФБИ, Лабораторная работа 1
+        </header>
+        
+        <h1>Web-server with Flask</h1>
+
+        <footer> &copy; Vadim Yushkov, FBI-11, 3rd year, 2023</footer>
+    </body>
+</html>
+"""
+
 
